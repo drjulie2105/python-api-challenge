4558d1ff515b6a2854c67ad2e94947d4c334fe73
Julie Pyle <drjuliepyle@gmail.com>
2020-08-31 13:52:09 -0400

    Created list of random cities

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "7203eacdc5ea51293732d8440f1cdf92"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "YAIzaSyC2CXibDLsLdS_3dXnJ-OwyBo2V8D5aw5c"
